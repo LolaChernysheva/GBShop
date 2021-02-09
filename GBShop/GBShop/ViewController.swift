8e242e04b6f2b6b3c4fd583cf4a4ce141e1d1dff
LolaChernysheva <lolita-chernysheva@mail.ru>
2021-02-09 17:21:29 +0300

    init GBShop

---
+++ GBShop/GBShop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GBShop
+//
+//  Created by Лолита on 09.02.2021.
+//  Copyright © 2021 Lolita Chernysheva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
